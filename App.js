8fe5666effad23a806e34055556b78cd830e0a90
jolenetan07 <56619993+jolenetan07@users.noreply.github.com>
2021-12-15 17:45:42 +0800

    add new task, delete finished task

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'complete myStars console app', key: '1' },
+    { text: 'design portfolio website', key: '2' },
+    { text: 'create myBTS app', key: '3' },
+    { text: 'drink coffee', key: '4' },
+    { text: 'watch crossed love', key: '5' },
+    { text: 'got7 weekly idol', key: '6' },
+    { text: 'in the soop season 2', key: '7' }
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random().toString() },
+        ...prevTodos
+      ];
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      {/* header */}
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
+        {/* add todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,7 +54,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+
+  },
+  list: {
+    marginTop: 20,
+
+  }
 });
---
+++ components/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Header() {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.title}>My Tasks</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: 'pink',
+        height: 120,
+        paddingTop: 60,
+        
+      },
+
+    title: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 31,
+        fontWeight: 'bold',
+
+    }
+});
+    
