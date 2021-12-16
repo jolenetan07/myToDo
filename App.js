78362c128d273feac70b84ee797fb81dcdf969a1
jolenetan07 <56619993+jolenetan07@users.noreply.github.com>
2021-12-16 13:19:48 +0800

    alert for short input and dismiss keyboard

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
@@ -22,31 +22,43 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos
-      ];
-    });
+    if(text.length > 5) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    } else {
+      Alert.alert('OOPS!', 'Tasks must be over 5 characters long', [
+        {text: 'Dismiss', onPress: () => console.log('close alert')}
+      ]);
+    }
+    
   }
 
   return (
-    <View style={styles.container}>
-      <Header />
-      {/* header */}
-      <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler} />
-        {/* add todo form */}
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      //console.log('keyboard dismissed')
+    }}>
+      <View style={styles.container}>
+        <Header />
+        {/* header */}
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          {/* add todo form */}
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
